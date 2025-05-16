c84c4d7fc50d5b4db12ba23a7bf127b81217489d
kao  hardcool kutoro <99511936+Badassdroid@users.noreply.github.com>
2025-05-17 00:32:16 +0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
     const fetchReviews = async () => {
       setIsLoading(true)
       try {
-        const response = await axios.get('http://127.0.0.1:5000/api/reviews')
+        const response = await axios.get('https://ai-solutions-backend.onrender.com/api/reviews')
         // Ensure we're working with an array of reviews
         if (response.data && Array.isArray(response.data)) {
           setReviews(response.data)
